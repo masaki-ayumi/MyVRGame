5edf4189bb9dc97ec28fee9ff5ef3b9b12cca7a4
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2021-10-14 12:49:18 +0900

    弾を前に進むことができた#1

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    Vector3 vector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+        vector = new Vector3(0.0f, 0.0f, 10.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.AddForce(vector);
+    }
+
+    public void IsShot()
+    {
+
+    }
+
+
+}
+
