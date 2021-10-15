d297e87fa1a583244e57f896fbbd8b80fefbfe03
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2021-10-15 15:57:37 +0900

    弾を発射できるようになった#1

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -6,27 +6,23 @@ public class Shot : MonoBehaviour
 {
     Rigidbody rigidbody;
     Vector3 vector;
+    float speed = 30f;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = this.GetComponent<Rigidbody>();
-        vector = new Vector3(0.0f, 0.0f, 100.0f);
+        vector = new Vector3(0.0f, 0.0f, 20.0f);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            rigidbody.AddForce(vector);
 
-        }
+        //rigidbody.velocity = transform.forward * speed;
+        rigidbody.velocity = vector;
+
 
 
     }
-
-
-
-
 }
 
