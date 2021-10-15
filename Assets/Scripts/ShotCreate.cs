306d6434f3760f80e6634b964ab070620126467e
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2021-10-15 15:58:43 +0900

    弾をPrefab化して複製#1

---
+++ Assets/Scripts/ShotCreate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotCreate : MonoBehaviour
+{
+    public GameObject shotCreate;
+    public GameObject shotPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GameObject shot = Instantiate(shotPrefab, shotCreate.transform);
+            Destroy(shot, 1.0f);
+        }
+    }
+}
