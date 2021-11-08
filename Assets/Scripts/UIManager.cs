9f71cceb8c2320e7afbd9da61b8fd37cb7c7c12f
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2021-11-08 14:38:43 +0900

    UI用のスクリプト作成　スコア表示実装#5

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    Text scoreText;
+
+
+    public GameObject target;
+    TargetController targetScripts;
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = this.GetComponent<Text>();
+
+        targetScripts = target.GetComponent<TargetController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = targetScripts.score;
+        scoreText.text = score.ToString();
+    }
+}
