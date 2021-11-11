0a16787432937229d40a77822ef8c3abf0950d8c
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2021-11-11 15:39:26 +0900

    的のターゲティング#6

---
+++ Assets/Scripts/TargetIndicator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetIndicator : MonoBehaviour
+{
+    public Transform target;
+    Camera mainCamera;
+    RectTransform rectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var centor = 0.5f * new Vector3(Screen.width, Screen.height);
+
+        var position = mainCamera.WorldToScreenPoint(target.position) - centor;
+
+        rectTransform.anchoredPosition = position;
+    }
+}
