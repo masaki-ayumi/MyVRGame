fa388b4a2d75cb7fefc156f640223a061e595568
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2021-11-15 16:01:16 +0900

    的のUIを追加#6

--- Assets/Scripts/TargetIndicator.cs
+++ Assets/Scripts/TargetIndicator.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TargetIndicator : MonoBehaviour
 {
     public Transform target;
     Camera mainCamera;
     RectTransform rectTransform;
+
+    public Image arrow;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +22,10 @@ public class TargetIndicator : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        var centor = 0.5f * new Vector3(Screen.width, Screen.height);
+        float canvasScale = transform.root.localScale.z;
+        var center = 0.5f * new Vector3(Screen.width, Screen.height);
 
-        var position = mainCamera.WorldToScreenPoint(target.position) - centor;
+        var position = mainCamera.WorldToScreenPoint(target.position) - center;
 
         // カメラ後方にあるターゲットのスクリーン座標は、画面中心に対する点対称の座標にする
         if (position.z < 0f)
@@ -30,6 +34,15 @@ public class TargetIndicator : MonoBehaviour
             position.y = -position.y;
         }
 
+        // カメラと水平なターゲットのスクリーン座標を補正する
+        if (Mathf.Approximately(position.y, 0f))
+        {
+            position.y = -center.y;
+        }
+
+
+
+        var halfSize = 0.5f * canvasScale * rectTransform.sizeDelta;
         float d = Mathf.Max(
             Mathf.Abs(position.x / position.x),
             Mathf.Abs(position.y / position.y)
@@ -43,10 +56,21 @@ public class TargetIndicator : MonoBehaviour
             position.y /= d;
         }
 
-        Debug.Log(position);
+        //Debug.Log(position);
         //Debug.Log(mainCamera.WorldToScreenPoint(target.position));
-        Debug.Log(centor);
+        //Debug.Log(center);
 
-        rectTransform.anchoredPosition = position;
+        rectTransform.anchoredPosition = position / canvasScale;
+
+
+        // ターゲットのスクリーン座標が画面外なら、ターゲットの方向を指す矢印を表示する
+        arrow.enabled = isOffscreen;
+        if (isOffscreen)
+        {
+            arrow.rectTransform.eulerAngles = new Vector3(
+                0f, 0f,
+                Mathf.Atan2(position.y, position.x) * Mathf.Rad2Deg
+            );
+        }
     }
 }
