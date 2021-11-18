b771a991add691c1c935b563c2f88b74b74f436a
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2021-11-18 11:32:40 +0900

    SE用スクリプト作成　銃声再生#8

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    AudioSource audioSource;
+    public AudioClip fireSE;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireSE()
+    {
+        audioSource.PlayOneShot(fireSE);
+    }
+}
