faec4151907870f6027453cd1a6878be98e2c8dd
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2021-12-16 15:29:40 +0900

    銃の回転をJoyConのジャイロに合わせるためのスクリプト#13

---
+++ Assets/Scripts/GunRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotation : MonoBehaviour
+{
+    List<Joycon> joycons;
+    Joycon joyconR;
+    Vector3 gyro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joycons = JoyconManager.Instance.j;
+        joyconR = joycons.Find(c => !c.isLeft);
+
+        gyro = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gyro = joyconR.GetGyro();
+    }
+}
