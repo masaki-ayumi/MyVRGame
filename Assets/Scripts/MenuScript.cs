5f2383db1ab789e6dabb75b7838644329c6a45c4
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2022-01-06 15:47:12 +0900

    追加：MenuScript.csを追加#16
    タイトルへ戻るメニュー画面のスクリプトを追加

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject menuObject;
+
+    GameObject uiManager;
+    UIManager uiManagerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManager = GameObject.FindGameObjectWithTag("UIManager");
+        uiManagerScript = uiManager.GetComponent<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (uiManagerScript.isStoopde)
+        {
+            menuObject.SetActive(true);
+        }
+    }
+}
