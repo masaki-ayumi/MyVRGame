d2e46b98b2b346a419cf5297159d18d646d8cd7f
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2022-01-17 13:08:07 +0900

    追加：MiniGameSwitch.csを追加#18
    的あてゲームを始めるためのスクリプト作成

---
+++ Assets/Scripts/MiniGameSwitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameSwitch : MonoBehaviour
+{
+    private GameObject uiManager;
+    private UIManager uiManagerScript;
+
+    private bool isStartMiniGame = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManager = GameObject.FindGameObjectWithTag("UIManager");
+        uiManagerScript = uiManager.GetComponent<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        isStartMiniGame = true;
+        uiManagerScript.isStartMiniGame = isStartMiniGame;
+    }
+}
