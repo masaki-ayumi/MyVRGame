841098acc4db285e41dad2e2b0c9aa8cb3ba292d
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2022-01-21 15:55:47 +0900

    追加：DOTweenを追加#18
    的あてゲーム用スイッチに動きを追加するためにDOTweenをインポート

---
+++ Assets/Scripts/DoTweenScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+/// <summary>
+/// 
+/// </summary>
+public class DoTweenScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.DORotate(new Vector3(180f, 0, 0), 2f).SetLoops(-1,LoopType.Restart);
+    }
+}
