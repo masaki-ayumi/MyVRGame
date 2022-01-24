318af8512bee38d2e5c1a98b1bf4bdc032710008
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2022-01-24 17:13:38 +0900

    追加：MiniGameStart.csを追加#18
    コリジョンをつかって弾が触れたかを判断するためのスクリプト

---
+++ Assets/Scripts/MiniGameStart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameStart : MonoBehaviour
+{
+    
+    private MiniGameSwitch parentScript;
+
+    private bool isHit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject parent = transform.parent.gameObject;
+        parentScript = parent.GetComponent<MiniGameSwitch>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        isHit = true;
+        parentScript.IsHit(isHit);
+        Debug.Log(isHit);
+        isHit = false;
+        parentScript.IsHit(isHit);
+
+        Debug.Log(isHit);
+
+    }
+}
