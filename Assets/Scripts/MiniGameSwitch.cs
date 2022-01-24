d2bffc35b6e997cbfd3cbba8e69a139e7c17627f
MatTyaN0125 <ilovegames.ilikesplatoon@gmail.com>
2022-01-24 17:11:09 +0900

    追加：的あてゲーム用スイッチのActiveについて追記　要修正#18
    ミニゲームが終わったらもう一度始められるようにしたいがうまくいっていない

--- Assets/Scripts/MiniGameSwitch.cs
+++ Assets/Scripts/MiniGameSwitch.cs
@@ -7,7 +7,11 @@ public class MiniGameSwitch : MonoBehaviour
     private GameObject uiManager;
     private UIManager uiManagerScript;
 
+    private GameObject switchObject;
+
     private bool isStartMiniGame = false;
+
+    //public bool isActive = true;
 
     private GameObject target;
     private Vector3 targetPosition;
@@ -24,31 +28,60 @@ public class MiniGameSwitch : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("Target");
         targetPosition = target.transform.position;
 
+        switchObject = GameObject.FindGameObjectWithTag("Switch");
+
         score.SetActive(false);
         time.SetActive(false);
+
+        switchObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(switchObject.activeSelf);
+        //switchObject.SetActive(isActive);
+
         if (uiManagerScript.isStoopde == true)
         {
-            this.gameObject.SetActive(true);
+            switchObject.SetActive(true);
+
+            score.SetActive(false);
+            time.SetActive(false);
         }
     }
 
-    public void OnTriggerEnter(Collider other)
+    public void IsHit(bool hit)
     {
-        target.transform.position = targetPosition;
+        if (hit)
+        {
+            uiManagerScript.isStoopde = false;
 
-        isStartMiniGame = true;
-        uiManagerScript.isStartMiniGame = isStartMiniGame;
+            target.transform.position = targetPosition;
 
-        score.SetActive(true);
-        time.SetActive(true);
+            isStartMiniGame = true;
+            uiManagerScript.isStartMiniGame = isStartMiniGame;
 
-        this.gameObject.SetActive(false);
+            score.SetActive(true);
+            time.SetActive(true);
 
+            switchObject.SetActive(false);
+        }
+        //switchObject.SetActive(true);
 
     }
+    //public void OnTriggerEnter(Collider other)
+    //{
+    //    target.transform.position = targetPosition;
+
+    //    isStartMiniGame = true;
+    //    uiManagerScript.isStartMiniGame = isStartMiniGame;
+
+    //    score.SetActive(true);
+    //    time.SetActive(true);
+
+    //    switchObject.SetActive(false);
+
+
+    //}
 }
